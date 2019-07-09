bb34755ceb737e10b59ee91b7d85a1e6510a1c49
Kid <kidonng@gmail.com>
2019-07-10 02:25:16 +0800

    feat: add apps search

---
+++ docs/.vuepress/enhanceApp.js
@@ -0,0 +1,11 @@
+import InstantSearch from 'vue-instantsearch'
+
+export default ({ Vue }) => {
+  Vue.use(InstantSearch)
+
+  // https://github.com/algolia/algoliasearch-client-javascript/issues/691
+  if (typeof process === 'undefined')
+    window.process = {
+      env: { DEBUG: null }
+    }
+}
