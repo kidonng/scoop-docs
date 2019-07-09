cdf73bc5018866ea44a42cbb919b9ba659c13a88
Kid <kidonng@gmail.com>
2019-07-10 02:59:15 +0800

    fix: fix client error

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -4,8 +4,8 @@ export default ({ Vue }) => {
   Vue.use(InstantSearch)
 
   // https://github.com/algolia/algoliasearch-client-javascript/issues/691
-  if (process.env.VUE_ENV !== 'server')
+  if (typeof process === 'undefined')
     window.process = {
-      env: { DEBUG: undefined }
+      env: { DEBUG: null }
     }
 }
