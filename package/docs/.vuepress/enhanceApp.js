743968dbfd51072c316ffdd27fafc05ac516702d
Kid <kidonng@gmail.com>
2019-07-19 11:19:34 +0800

    fix: fix SSR

--- package/docs/.vuepress/enhanceApp.js
+++ package/docs/.vuepress/enhanceApp.js
@@ -5,19 +5,20 @@ export default ({ Vue, router }) => {
   Vue.use(plugin)
   Vue.use(InstantSearch)
 
-  // https://github.com/algolia/algoliasearch-client-javascript/issues/691
-  if (typeof process === 'undefined')
+  if (typeof process === 'undefined') {
+    // https://github.com/algolia/algoliasearch-client-javascript/issues/691
     window.process = {
       env: { DEBUG: false }
     }
 
-  // Handle DocSearch links
-  // Pending: https://github.com/vuejs/vuepress/pull/1706
-  document.addEventListener('click', e => {
-    const target = e.target.closest('.algolia-docsearch-suggestion')
-    if (target) {
-      e.preventDefault()
-      router.push(target.href.replace('https://scoop-docs.now.sh', ''))
-    }
-  })
+    // Handle DocSearch links
+    // Pending: https://github.com/vuejs/vuepress/pull/1706
+    document.addEventListener('click', e => {
+      const target = e.target.closest('.algolia-docsearch-suggestion')
+      if (target) {
+        e.preventDefault()
+        router.push(target.href.replace('https://scoop-docs.now.sh', ''))
+      }
+    })
+  }
 }
