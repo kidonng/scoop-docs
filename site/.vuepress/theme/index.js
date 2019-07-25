537e8b79148cdb124ab0087bf015b826d0748733
Kid <kidonng@gmail.com>
2019-07-25 23:27:02 +0800

    refactor: use external latest theme

--- site/.vuepress/theme/index.js
+++
@@ -1,5 +0,0 @@
-// This is not a custom theme, it's extracted to remove the default code theme
-// Pending: https://github.com/vuejs/vuepress/pull/1707
-module.exports = {
-  extend: '@vuepress/theme-default'
-}
