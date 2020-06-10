77fee36d8347a78cfa364b68a84b3299cb5e20e9
Kid <kidonng@gmail.com>
2020-06-10 18:17:54 +0800

    Fix sidebar

--- .vuepress/utils/sidebar.js
+++ .vuepress/utils/sidebar.js
@@ -6,8 +6,8 @@ const groups = [
     children: [
       'So-What.md',
       'Chocolatey-Comparison.md',
-      'Cygwin-and-MSYS-Comparison.md'
-    ]
+      'Cygwin-and-MSYS-Comparison.md',
+    ],
   },
   {
     title: 'getting-started',
@@ -15,26 +15,27 @@ const groups = [
       'Quick-Start.md',
       'Commands.md',
       'FAQ.md',
-      'Uninstalling-Scoop.md'
-    ]
+      'Uninstalling-Scoop.md',
+    ],
   },
   { title: 'concepts' },
   { title: 'guides' },
-  { title: 'misc' }
+  { title: 'misc' },
 ]
 
-groups.forEach(async group => {
-  if (group.children)
-    group.children = group.children.map(page => `docs/${group.title}/${page}`)
-  else {
-    const pages = await globby(`docs/docs/${group.title}`)
-    group.children = pages.map(page => page.replace('docs/docs', 'docs'))
-  }
+groups.forEach(async (group) => {
+  const { children, title } = group
 
-  group.title = group.title
+  group.children = children
+    ? children.map((page) => `docs/${title}/${page}`)
+    : await globby(`docs/${title}`)
+
+  group.title = title
     .split('-')
-    .map(str => `${str.charAt(0).toUpperCase()}${str.substring(1)}`)
+    .map((str) => `${str.charAt(0).toUpperCase()}${str.substring(1)}`)
     .join(' ')
+
+  group.collapsable = false
 })
 
 module.exports = groups
